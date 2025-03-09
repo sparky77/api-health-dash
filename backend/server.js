652414e5bbd4b4b9bdf4550dba4ab2ad26da4bf3
Marcus Smith <marcus@Marcuss-MacBook-Pro.local>
2025-03-09 18:58:06 +0000

    axios depen added to backend package

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const cors = require('cors');
 app.use(cors({
   origin: ['https://main--resilient-kangaroo-52fcb1.netlify.app', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
